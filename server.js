286008f8727cd29b895f397c4c915862899de3f3
davidnherran <davidnherran@gmail.com>
2020-06-15 22:22:41 -0500

    Login passport finalizado

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require("express");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const PassportLocal = require("passport-local").Strategy;
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser("top secret token"));
+
+app.use(
+  session({
+    secret: "confirm secret token",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new PassportLocal(function (username, password, done) {
+    //done(err,{name: "David"},{})
+    //Conexion a base de datos - se realizara hardcode con fines ilustrativos
+    if (username === "davidnherran" && password === "12345")
+      return done(null, { id: 1, name: "David" });
+
+    done(null, false);
+  })
+);
+
+//{ id: 1, name: "David" }
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, { id: 1, name: "David" });
+});
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/", (req,res,next)=>{
+  if(req.isAuthenticated()) return next();
+
+  res.redirect("/login");
+},(req, res) => {
   //Si inicio sesion, mostrara bienvenida
+  res.send("Proyecto inicializado correctamente");
   //Si no inicia sesion, redireccionara a /login
 });
 
@@ -13,8 +59,12 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  //recibir credenciales e iniciar sesion
-});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 app.listen(8080, () => console.log("Server started"));
