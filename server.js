c92d9d7e11f258250bf1ae6363b50871445f3c25
davidnherran <davidnherran@gmail.com>
2020-06-15 20:30:58 -0500

    Configuración inicial de express en server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //Si inicio sesion, mostrara bienvenida
+  //Si no inicia sesion, redireccionara a /login
+});
+
+app.get("/login", (req, res) => {
+  //mostrar formulario de login
+  res.send("hola mundo");
+});
+
+app.post("/login", (req, res) => {
+  //recibir credenciales e iniciar sesion
+});
+
+app.listen(8080, () => console.log("Server started"));
